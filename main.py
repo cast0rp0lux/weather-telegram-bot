03481a9332a35404f1c73c32cbc60df7f4544a6a
cast0rp0lux <albertoaranamo@gmail.com>
2026-03-15 11:12:46 +0100

    Actualizar main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "TU_TOKEN_AQUI"
+import os
+TOKEN = os.environ["BOT_TOKEN"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot funcionando")
